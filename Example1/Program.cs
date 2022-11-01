c2af55cc68d34f810ae89fc5d2b7c1f6ca63c622
alya <alya-892@yandex.ru>
2022-11-01 11:11:08 +0300

    Создание репозитория

---
+++ Example1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите первое число");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0 ;
+int min = 0;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a ;
+}
+Console.Write ("Max = ");
+Console.WriteLine (max);
+Console.Write ("Min = ");
+Console.WriteLine (min);  
---
+++ Example4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+      
+for (int a = 1; a <= N; a++ )
+    
+    if (a % 2 == 0)
+{
+Console.WriteLine("{0}", a);
+}
